e005669dcb2028c410089a1d4c8d48f109313350
Karapuz1982 <121318106+Karapuz1982@users.noreply.github.com>
2023-07-30 18:45:16 +0200

    change phonebook

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,77 +1,77 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Label, Input, Button } from './Phonebook styled';
 
-class ContactForm extends Component {
-    static propTypes = {
-      
-    onAddContact: PropTypes.func.isRequired,
-  };
+const ContactForm = ({ onAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [isNameValid, setIsNameValid] = useState(true);
+  const [isNumberValid, setIsNumberValid] = useState(true);
 
-  state = {
-    name: '',
-      number: '',
-      isNameValid: true,
-      isNumberValid: true
-  };
-
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const { name, value } = event.target;
-      this.setState({ [name]: value, isNameValid: true, isNumberValid: true  });
+    if (name === 'name') {
+      setName(value);
+      setIsNameValid(true);
+    } else if (name === 'number') {
+      setNumber(value);
+      setIsNumberValid(true);
+    }
   };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        const { name, number } = this.state;
-        const namePattern = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
-        if (!name || !namePattern.test(name)) {
-            this.setState({ isNameValid: false });
-            return;
-        }
-        const numberPattern = /^[0-9]{3}-[0-9]{2}-[0-9]{2}$/;
-        if (!number || !numberPattern.test(number)) {
-            this.setState({ isNumberValid: false });
-            return;
-        }
-            this.props.onAddContact(name, number);
-            this.setState({ name: '', number: '' });
-        };
-    
-    
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const namePattern = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
+    const numberPattern = /^[0-9]{3}-[0-9]{2}-[0-9]{2}$/;
 
-        render() {
-            const { name, number, isNameValid, isNumberValid } = this.state;
-
-            return (
-                <Form onSubmit={this.handleSubmit}>
-                    <Label>
-                        Name
-                        <Input
-                            type="text"
-                            name="name"
-                            value={name}
-                            onChange={this.handleChange}
-                            $isValid={isNameValid}
-                            required
-                        />
-                        {!isNameValid && <div style={{ color: 'red' }}>Please enter a valid name</div>}
-                    </Label>
-                    <Label>
-                        Number
-                        <Input
-                            type="tel"
-                            name="number"
-                            value={number}
-                            onChange={this.handleChange}
-                            $isValid={isNumberValid}
-                            required
-                        />
-                        {!isNumberValid && <div style={{ color: 'red' }}>Please enter a valid number XXX-XX-XX</div>}
-                    </Label>
-                    <Button type="submit">Add contact</Button>
-                </Form>
-            );
-        }
+    if (!name || !namePattern.test(name)) {
+      setIsNameValid(false);
+      return;
     }
+
+    if (!number || !numberPattern.test(number)) {
+      setIsNumberValid(false);
+      return;
+    }
+
+    onAddContact(name, number);
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Label>
+        Name
+        <Input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          $isValid={isNameValid}
+          required
+        />
+        {!isNameValid && <div style={{ color: 'red' }}>Please enter a valid name</div>}
+      </Label>
+      <Label>
+        Number
+        <Input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          $isValid={isNumberValid}
+          required
+        />
+        {!isNumberValid && <div style={{ color: 'red' }}>Please enter a valid number XXX-XX-XX</div>}
+      </Label>
+      <Button type="submit">Add contact</Button>
+    </Form>
+  );
+};
+
+ContactForm.propTypes = {
+  onAddContact: PropTypes.func.isRequired,
+};
 
 export default ContactForm;
